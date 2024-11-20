c312173708f685693c43b6eb5dc9ecaccaab6b38
Shanoah Alkire <arcum42@gmail.com>
2024-11-19 22:25:50 -0800

    Added in Lora Stack nodes and a node to apply them to the model and clip.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,30 @@
 from .sage import *
 
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "Sage_LoraStackDebugString": Sage_LoraStackDebugString,
+    "Sage_LoraStack": Sage_LoraStack,
+    "Sage_LoraStackLoader": Sage_LoraStackLoader,
+    "Sage_DualCLIPTextEncode": Sage_DualCLIPTextEncode,
+    "Sage_SamplerInfo": Sage_SamplerInfo,
+    "Sage_KSampler": Sage_KSampler,
+    "Sage_ConstructMetadata": Sage_ConstructMetadata,
+    "Sage_CheckpointLoaderSimple": Sage_CheckpointLoaderSimple,
+    "Sage_SaveImageWithMetadata": Sage_SaveImageWithMetadata
+}
+ 
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS  = {
+    "Sage_LoraStackDebugString": "Lora Stack Debug String",
+    "Sage_LoraStackLoader": "Lora Stack Loader",
+    "Sage_LoraStack":   "Simple Lora Stack",
+    "Sage_DualCLIPTextEncode": "Prompts to CLIP",
+    "Sage_SamplerInfo": "Sampler Info",
+    "Sage_KSampler": "KSampler w/ Sampler Info",
+    "Sage_ConstructMetadata": "Construct Metadata",
+    "Sage_CheckpointLoaderSimple": "Load Checkpoint With Name",
+    "Sage_SaveImageWithMetadata": "Save Image with Added Metadata"
+}
+
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS'] 
---
+++ sage_utils.py
@@ -0,0 +1,54 @@
+#Utility functions for use in the nodes.
+
+import pathlib
+import hashlib
+
+def lora_to_string(lora_name, model_weight, clip_weight):
+    lora_string = ' <lora:' + str(pathlib.Path(lora_name).name) + ":" + str(model_weight) + ":"  + str(clip_weight) +  ">"
+        
+    return lora_string
+
+def lora_stack_to_string(stack):
+    lora_string = ''
+        
+    for lora in stack:
+        lora_string += lora_to_string(lora[0], lora[1], lora[2])
+        
+    return lora_string
+
+def get_file_sha256(path):
+    m = hashlib.sha256()
+    with open(path, 'rb') as f:
+        m.update(f.read())
+    result = str(m.digest().hex()[:10])
+    return result
+
+def civitai_sampler_name(sampler_name, scheduler_name):
+    comfy_to_auto = {
+        'ddim': 'DDIM',
+        'dpm_2': 'DPM2',
+        'dpm_2_ancestral': 'DPM2 a',
+        'dpmpp_2s_ancestral': 'DPM++ 2S a',
+        'dpmpp_2m': 'DPM++ 2M',
+        'dpmpp_sde': 'DPM++ SDE',
+        'dpmpp_2m_sde': 'DPM++ 2M SDE',
+        'dpmpp_3m_sde': 'DPM++ 3M SDE',
+        'dpm_fast': 'DPM fast',
+        'dpm_adaptive': 'DPM adaptive',
+        'euler_ancestral': 'Euler a',
+        'euler': 'Euler',
+        'heun': 'Heun',
+        'lcm': 'LCM',
+        'lms': 'LMS',
+        'plms': 'PLMS',
+        'uni_pc': 'UniPC',
+        'uni_pc_bh2': 'UniPC'
+    }
+    result = comfy_to_auto[sampler_name]
+    
+    if (scheduler_name == "karras"):
+        result += " Karras"
+    elif (scheduler_name == "exponential"):
+        result += " Exponential"
+    
+    return result
