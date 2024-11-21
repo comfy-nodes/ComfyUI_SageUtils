f43836abf9e6fb35129bfbe490e0fd4116fe11ed
Shanoah Alkire <arcum42@gmail.com>
2024-11-20 17:21:11 -0800

    Add toggles to turn on and off the comfyui metadata when saving.

--- sage.py
+++ sage.py
@@ -421,7 +421,9 @@ class Sage_SaveImageWithMetadata:
         return {
             "required": {
                 "images": ("IMAGE", {"tooltip": "The images to save."}),
-                "filename_prefix": ("STRING", {"default": "ComfyUI_Meta", "tooltip": "The prefix for the file to save. This may include formatting information such as %date:yyyy-MM-dd% or %Empty Latent Image.width% to include values from nodes."})
+                "filename_prefix": ("STRING", {"default": "ComfyUI_Meta", "tooltip": "The prefix for the file to save. This may include formatting information such as %date:yyyy-MM-dd% or %Empty Latent Image.width% to include values from nodes."}),
+                "include_node_metadata": ("BOOLEAN", {"default": True, "defaultInput": False}),
+                "include_extra_pnginfo_metadata": ("BOOLEAN", {"default": True, "defaultInput": False})
             },
             "optional": {
                 "param_metadata": ("STRING",{ "defaultInput": True}),
@@ -440,30 +442,32 @@ class Sage_SaveImageWithMetadata:
     CATEGORY = "Sage Utils"
     DESCRIPTION = "Saves the input images to your ComfyUI output directory with added metadata. The param_metadata input should come from Construct Metadata, and the extra_metadata is anything you want."
 
-    def set_metadata(self, param_metadata = None, extra_metadata=None, prompt=None, extra_pnginfo=None):
+    def set_metadata(self, include_node_metadata, include_extra_pnginfo_metadata, param_metadata = None, extra_metadata=None, prompt=None, extra_pnginfo=None):
         result = None
         if not cli_args.args.disable_metadata:
             result = PngInfo()
             if param_metadata is not None:
                 result.add_text("parameters", param_metadata)
-            if prompt is not None:
-                result.add_text("prompt", json.dumps(prompt))
-            if extra_pnginfo is not None:
-                for x in extra_pnginfo:
+            if include_node_metadata == True:
+                if prompt is not None:
+                    result.add_text("prompt", json.dumps(prompt))
+            if include_extra_pnginfo_metadata == True:
+                if extra_pnginfo is not None:
+                    for x in extra_pnginfo:
                         result.add_text(x, json.dumps(extra_pnginfo[x]))
             if extra_metadata is not None:
                 result.add_text("Extra", extra_metadata)
         return result
         
 
-    def save_images(self, images, filename_prefix="ComfyUI", param_metadata = None, extra_metadata=None, prompt=None, extra_pnginfo=None):
+    def save_images(self, images, filename_prefix, include_node_metadata, include_extra_pnginfo_metadata, param_metadata = None, extra_metadata=None, prompt=None, extra_pnginfo=None):
         filename_prefix += self.prefix_append
         full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir, images[0].shape[1], images[0].shape[0])
         results = list()
         for (batch_number, image) in enumerate(images):
             i = 255. * image.cpu().numpy()
             img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
-            final_metadata = self.set_metadata(param_metadata, extra_metadata, prompt, extra_pnginfo)
+            final_metadata = self.set_metadata(include_node_metadata, include_extra_pnginfo_metadata, param_metadata, extra_metadata, prompt, extra_pnginfo)
 
             filename_with_batch_num = filename.replace("%batch_num%", str(batch_number))
             file = f"{filename_with_batch_num}_{counter:05}_.png"
