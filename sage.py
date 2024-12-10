9f80560879288531fd0fde10b258101d3f0023bc
arcum42 <arcum42@gmail.com>
2024-12-10 08:14:02 -0800

    Add toggle to lora stack to enable or disable them. (May need to recreate node in existing workflows.)

--- sage.py
+++ sage.py
@@ -403,6 +403,7 @@ class Sage_LoraStack:
     def INPUT_TYPES(s):
         return {
             "required": {
+                "enabled": ("BOOLEAN", {"defaultInput": False, "default": True}),
                 "lora_name": (folder_paths.get_filename_list("loras"), {"defaultInput": False, "tooltip": "The name of the LoRA."}),
                 "model_weight": ("FLOAT", {"defaultInput": False, "default": 1.0, "min": -100.0, "max": 100.0, "step": 0.01, "tooltip": "How strongly to modify the diffusion model. This value can be negative."}),
                 "clip_weight": ("FLOAT", {"defaultInput": False, "default": 1.0, "min": -100.0, "max": 100.0, "step": 0.01, "tooltip": "How strongly to modify the CLIP model. This value can be negative."}),
@@ -419,8 +420,11 @@ class Sage_LoraStack:
     CATEGORY = "Sage Utils/lora"
     DESCRIPTION = "Choose a lora with weights, and add it to a lora_stack. Compatable with other node packs that have lora_stacks."
     
-    def add_lora_to_stack(self, lora_name, model_weight, clip_weight, lora_stack = None):
-        stack = add_lora_to_stack(lora_name, model_weight, clip_weight, lora_stack)
+    def add_lora_to_stack(self, enabled, lora_name, model_weight, clip_weight, lora_stack = None):
+        if enabled == True:
+            stack = add_lora_to_stack(lora_name, model_weight, clip_weight, lora_stack)
+        else:
+            stack = lora_stack
 
         return (stack,)
 
